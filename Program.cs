f7ea1bd48df7f864c389a11b1410569aee802173
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-15 19:18:09 -0300

    exercicíos resolvidos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE SEU NOME! por favor");
+
+            string nome;
+            nome = Console.ReadLine();
+            Console.WriteLine("Obrigadaaa," + nome + "!!!!!");
+
+        }
+    }
+}
